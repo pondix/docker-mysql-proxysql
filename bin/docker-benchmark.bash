a7c837d465f4988b2cedadbcdb17f7b5aa0c492b
Nikolaos Vyzas <pondix@gmail.com>
2018-02-14 03:30:25 +0200

    Adding drop / create of dataset as default for benchmark

--- bin/docker-benchmark.bash
+++ bin/docker-benchmark.bash
@@ -7,6 +7,9 @@ SIZE_TABLES=10000
 REPORT_INTERVAL=5
 TIME=60
 
+printf "$RED[$(date)] Dropping 'sysbench' schema if present and preparing test dataset:$NORMAL\n"
+mysql -h127.0.0.1 -P16033 -uroot -proot -e"DROP DATABASE IF EXISTS sysbench; CREATE DATABASE IF NOT EXISTS sysbench"
+
 printf "$POWDER_BLUE[$(date)] Running Sysbench Benchmarksi against ProxySQL:"
 sysbench /usr/share/sysbench/oltp_read_write.lua --table-size=$SIZE_TABLES --tables=$NUM_TABLES --threads=$PREP_THREADS \
  --mysql-db=sysbench --mysql-user=root --mysql-password=$MYSQL_PWD --mysql-host=127.0.0.1 --mysql-port=16033 prepare
