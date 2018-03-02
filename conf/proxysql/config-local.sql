a8c75473eedb46ab40d2393e7813f4556f5aaf0d
Nikolaos Vyzas <pondix@gmail.com>
2018-03-02 16:59:15 +0200

    Adding config to populate local instance

---
+++ conf/proxysql/config-local.sql
@@ -0,0 +1,20 @@
+DELETE FROM mysql_servers;
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag,comment) VALUES (0,'127.0.0.1',13306,1,'mysql1');
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag,comment) VALUES (1,'127.0.0.1',13306,1,'mysql1');
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag,comment) VALUES (1,'127.0.0.1',13307,1,'mysql2');
+INSERT INTO mysql_servers (hostgroup_id,hostname,port,max_replication_lag,comment) VALUES (1,'127.0.0.1',13308,1,'mysql3');
+DELETE FROM mysql_replication_hostgroups;
+INSERT INTO mysql_replication_hostgroups VALUES (0,1,'');
+LOAD MYSQL SERVERS TO RUNTIME;
+SAVE MYSQL SERVERS TO DISK;
+
+DELETE FROM mysql_users;
+INSERT INTO mysql_users (username,password,active) values ('root','root',1);
+LOAD MYSQL USERS TO RUNTIME;
+SAVE MYSQL USERS TO DISK; 
+
+DELETE FROM mysql_query_rules;
+INSERT INTO mysql_query_rules (rule_id,active,match_digest,destination_hostgroup,apply) VALUES (1,1,'^SELECT.*FOR UPDATE',0,1),(2,1,'^SELECT',1,1);
+LOAD MYSQL QUERY RULES TO RUNTIME;
+SAVE MYSQL QUERY RULES TO DISK;
+
