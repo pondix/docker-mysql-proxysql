8f614ec47b6d0401fd6b3881090f2479a6258be5
Nikolaos Vyzas <pondix@gmail.com>
2018-04-22 19:01:34 +0300

    Adding libboost-1.55 as this is the version the binlog reader is compiled with

--- docker-compose-init.bash
+++ docker-compose-init.bash
@@ -12,7 +12,12 @@ sleep 1
 docker-compose up -d
 ./bin/docker-mysql-post.bash && ./bin/docker-proxy-post.bash && ./bin/docker-orchestrator-post.bash
 
+### Copy prerequisites
+docker cp conf/proxysql/binaries/libboost-system1.55.0_1.55.0+dfsg-3_amd64.deb docker-mysql-proxysql_mysql1_1:/root/
+docker cp conf/proxysql/binaries/libboost-system1.55.0_1.55.0+dfsg-3_amd64.deb docker-mysql-proxysql_mysql2_1:/root/
+docker cp conf/proxysql/binaries/libboost-system1.55.0_1.55.0+dfsg-3_amd64.deb docker-mysql-proxysql_mysql3_1:/root/
+
 ### Start binlog_reader
-docker-compose exec mysql1 bin/bash -c "apt update && apt -y install libboost-system1.55.0 && /opt/proxysql_mysqlbinlog/proxysql_binlog_reader -h 127.0.0.1 -u root -p root -P 3306 -l 999 -L /var/log/binlog_reader.log"
-docker-compose exec mysql2 bin/bash -c "apt update && apt -y install libboost-system1.55.0 && /opt/proxysql_mysqlbinlog/proxysql_binlog_reader -h 127.0.0.1 -u root -p root -P 3306 -l 999 -L /var/log/binlog_reader.log"
-docker-compose exec mysql3 bin/bash -c "apt update && apt -y install libboost-system1.55.0 && /opt/proxysql_mysqlbinlog/proxysql_binlog_reader -h 127.0.0.1 -u root -p root -P 3306 -l 999 -L /var/log/binlog_reader.log"
+docker-compose exec mysql1 bin/bash -c "apt update && apt install libssl1.0.2 && ln -s /usr/lib/x86_64-linux-gnu/libssl.so.1.0.2 /usr/lib/x86_64-linux-gnu/libssl.so.1.0.0 && ln -s /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.2 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.0 && dpkg -i /root/libboost-system1.55.0_1.55.0+dfsg-3_amd64.deb && /opt/proxysql_mysqlbinlog/proxysql_binlog_reader -h 127.0.0.1 -u root -p root -P 3306 -l 999 -L /var/log/binlog_reader.log"
+docker-compose exec mysql2 bin/bash -c "apt update && apt install libssl1.0.2 && ln -s /usr/lib/x86_64-linux-gnu/libssl.so.1.0.2 /usr/lib/x86_64-linux-gnu/libssl.so.1.0.0 && ln -s /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.2 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.0 && dpkg -i /root/libboost-system1.55.0_1.55.0+dfsg-3_amd64.deb && /opt/proxysql_mysqlbinlog/proxysql_binlog_reader -h 127.0.0.1 -u root -p root -P 3306 -l 999 -L /var/log/binlog_reader.log"
+docker-compose exec mysql3 bin/bash -c "apt update && apt install libssl1.0.2 && ln -s /usr/lib/x86_64-linux-gnu/libssl.so.1.0.2 /usr/lib/x86_64-linux-gnu/libssl.so.1.0.0 && ln -s /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.2 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.0.0 && dpkg -i /root/libboost-system1.55.0_1.55.0+dfsg-3_amd64.deb && /opt/proxysql_mysqlbinlog/proxysql_binlog_reader -h 127.0.0.1 -u root -p root -P 3306 -l 999 -L /var/log/binlog_reader.log"
