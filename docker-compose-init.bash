a641243a237d00b75ab893a28a696c8476f83c7d
René Cannaò <rene.cannao@gmail.com>
2019-04-22 15:03:14 +1000

    Reconfigure proxysql at the very end

--- docker-compose-init.bash
+++ docker-compose-init.bash
@@ -10,7 +10,7 @@ printf "$NORMAL"
 sleep 1
 
 docker-compose up -d
-./bin/docker-mysql-post.bash && ./bin/docker-proxy-post.bash && ./bin/docker-orchestrator-post.bash && ./bin/docker-restart-binlog_reader.bash
+./bin/docker-mysql-post.bash && ./bin/docker-orchestrator-post.bash && ./bin/docker-restart-binlog_reader.bash && ./bin/docker-proxy-post.bash
 
 if [[ -z "$1" ]]; then
     ./bin/docker-benchmark.bash
